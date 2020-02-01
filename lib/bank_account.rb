89f23c1066b8d5cf6bd9d26ba4f834cc9c29e131
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 23:00:49 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,28 +1,28 @@
-class BankAccount
+# class BankAccount
   
-  attr_reader :name 
-  attr_accessor :balance, :status
+#   attr_reader :name 
+#   attr_accessor :balance, :status
   
-  def initialize(name)
-    @name = name 
-    @balance = 1000
-    @status = "open"
-  end 
+#   def initialize(name)
+#     @name = name 
+#     @balance = 1000
+#     @status = "open"
+#   end 
   
-  def deposit(amount)
-    self.balance += amount 
-  end 
+#   def deposit(amount)
+#     self.balance += amount 
+#   end 
   
-  def display_balance 
-    "Your balance is $#{self.balance}."
-  end 
+#   def display_balance 
+#     "Your balance is $#{self.balance}."
+#   end 
   
-  def valid?
-    self.status == "open" && self.balance > 0 
-  end 
+#   def valid?
+#     self.status == "open" && self.balance > 0 
+#   end 
   
-  def close_account 
-    self.status = "closed" 
-  end 
+#   def close_account 
+#     self.status = "closed" 
+#   end 
   
-end
+# end
