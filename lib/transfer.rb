4ea0375c6e1dc2898839740fac6a932a724fd52d
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 23:09:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -67,10 +67,12 @@
 class Transfer 
   
   attr_reader :sender, :receiver 
+  attr_accessor :status 
   
   def initialize(sender, receiver)
     @sender = sender 
     @receiver = receiver 
+    @status = "pending" 
   end 
   
 end 
