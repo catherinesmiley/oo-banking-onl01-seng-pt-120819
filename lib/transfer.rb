4f2136ef18cfbaa6af9e772f06b7caab43a85ba9
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 23:00:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,38 +1,38 @@
-require 'pry'
+# require 'pry'
 
-class Transfer 
+# class Transfer 
   
-  attr_accessor :status
-  attr_reader :sender, :receiver, :amount
+#   attr_accessor :status
+#   attr_reader :sender, :receiver, :amount
   
-  def initialize(sender, receiver, amount)
-    @sender = sender 
-    @receiver = receiver 
-    @status = "pending"
-    @amount = 50
-  end 
+#   def initialize(sender, receiver, amount)
+#     @sender = sender 
+#     @receiver = receiver 
+#     @status = "pending"
+#     @amount = 50
+#   end 
   
-  def valid? 
-    sender.valid? && receiver.valid?
-  end 
+#   def valid? 
+#     sender.valid? && receiver.valid?
+#   end 
   
-  def execute_transaction
-    if !self.valid? || self.status == "complete"
-      self.status = "rejected"
-      sender.close_account
-      "Transaction rejected. Please check your account balance."
-    else 
-      sender.balance -= self.amount
-      receiver.balance += self.amount
-      self.status = "complete"
-    end 
-  end 
+#   def execute_transaction
+#     if !self.valid? || self.status == "complete"
+#       self.status = "rejected"
+#       sender.close_account
+#       "Transaction rejected. Please check your account balance."
+#     else 
+#       sender.balance -= self.amount
+#       receiver.balance += self.amount
+#       self.status = "complete"
+#     end 
+#   end 
   
-  def reverse_transfer
-    if self.status == "complete"
-      sender.balance += self.amount 
-      receiver.balance -= self.amount 
-      self.status = "reversed"
-    end
-  end 
-end 
+#   def reverse_transfer
+#     if self.status == "complete"
+#       sender.balance += self.amount 
+#       receiver.balance -= self.amount 
+#       self.status = "reversed"
+#     end
+#   end 
+# end 
